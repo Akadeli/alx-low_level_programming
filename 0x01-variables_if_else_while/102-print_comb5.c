d339b9d569623a06ecf2371407b869dfaa2054ae
Akadeli <hannekiru@gmail.com>
2022-09-12 08:01:17 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,9 +20,9 @@ int main(void)
 			putchar((x / 10) + 48);
 			putchar((x % 10) + 48);
 			putchar(' ');
-			putchar((y / 10) + 48);								
+			putchar((y / 10) + 48);
 			putchar((y % 10) + 48);
-		if (x != 98 || y != 99)
+			if (x != 98 || y != 99)
 		{			
 			putchar(',');
 			putchar(' ');
